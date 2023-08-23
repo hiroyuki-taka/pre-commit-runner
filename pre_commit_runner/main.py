9a2f84eb64ddd55d67a12cdaf0c3bcc2aaec706e
taka <taka@fullmarks.co.jp>
2023-08-23 13:57:31 +0900

    - PATHに実在しないディレクトリが入っていた場合に対応

--- pre_commit_runner/main.py
+++ pre_commit_runner/main.py
@@ -17,15 +17,17 @@ def main():
     path_items = os.environ.get("PATH", "").split(os.path.pathsep)
     run_with_shell = True
     for path_item in path_items:
-        print(f"check-path: {path_item}")
-        poetry_execute_file = [file for file in pathlib.Path(path_item).iterdir() if file.is_file() and file.name.startswith("poetry")]
-        pre_commit_execute_file = [file for file in pathlib.Path(path_item).iterdir() if file.is_file() and file.name.startswith("pre-commit")]
+        target_path = pathlib.Path(path_item)
+        if target_path.is_dir():
+            print(f"check-path: {path_item}")
+            poetry_execute_file = [file for file in target_path.iterdir() if file.is_file() and file.name.startswith("poetry")]
+            pre_commit_execute_file = [file for file in target_path.iterdir() if file.is_file() and file.name.startswith("pre-commit")]
 
-        if poetry_execute_file and pre_commit_execute_file:
-            print(f"find: {poetry_execute_file[0].name}")
-            print(f"find: {pre_commit_execute_file[0].name}")
-            run_with_shell = False
-            break
+            if poetry_execute_file and pre_commit_execute_file:
+                print(f"find: {poetry_execute_file[0].name}")
+                print(f"find: {pre_commit_execute_file[0].name}")
+                run_with_shell = False
+                break
 
     if files:
         target_pwd = repository_root / sys.argv[1]
